29d6acc3ad9316480d5f4b02344909ff7fecd0ca
Daniel Grundel <grundeld@amazon.com>
2021-06-03 16:36:43 -0700

    custom config tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -8,6 +8,27 @@ describe('outie', () => {
         expect(actual).toStrictEqual(expected);
     });
 
+    it('should suport custom start and end sequences', () => {
+        const outie = new Outie({
+            tokenStart: '<%',
+            tokenEnd: '%>',
+        });
+        const actual = outie.render('Hello, <%name%>!', { name: 'world' });
+        const expected = 'Hello, world!';
+        expect(actual).toStrictEqual(expected);
+    });
+
+    it('should suport custom raw identifier sequences', () => {
+        const outie = new Outie({
+            tokenStart: '<%',
+            tokenEnd: '%>',
+            rawTokenIdentifier: 'raw'
+        });
+        const actual = outie.render('Hello, <%rawname%>!', { name: '<b>world</b>' });
+        const expected = 'Hello, <b>world</b>!';
+        expect(actual).toStrictEqual(expected);
+    });
+
     it('should prevent xss', () => {
         const outie = new Outie();
         const actual = outie.render('Hello, {name}!', { name: '<script>alert("xss!");</script>' });
